5b3c0d7e51e2e8981924eecd7a647f714121f246
SanjuManna <sanjumanna2201@gmail.com>
2023-05-15 21:39:18 +0530

    Commit

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,6 @@
-const Users   = require("../models/user.js");
+const Users = require("../models/user.js");
 const jwt = require("jsonwebtoken");
-const bcrypt  = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 const saltRounds = 10;
 const JWT_SECRET = "newtonSchool";
@@ -57,12 +57,44 @@ json = {
 
 */
 
-const loginUser =async (req, res) => {
+const loginUser = async (req, res) => {
 
-    const email  = req.body.email;
-    const password = req.body.password;
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
 
-    //Write your code here.
+        //Write your code here.
+        const user = await Users.findOne({ 'email': email });
+        if (!user) {
+            res.status(404).json({
+                "message": 'User with this E-mail does not exist !!',
+                "status": 'fail'
+            })
+        }
+        const passwordMatch = bcrypt.compareSync(password, user.password);
+        if (!passwordMatch) {
+            res.status(403).json({
+                "message": 'Invalid Password, try again !!',
+                "status": 'fail'
+            })
+        }
+        const token = jwt.sign(
+            { userId: user._id },
+            JWT_SECRET,
+            {
+                expiresIn: "1h"
+            }
+        )
+        res.status(200).json({
+            status: 'success',
+            token: token
+        })
+
+    } catch (err) {
+
+    }
+
+
 
 }
 
@@ -117,11 +149,42 @@ json = {
 
 const signupUser = async (req, res) => {
 
-    const {email, password, name, role} = req.body;
+    try {
+        const { email, password, name, role } = req.body;
 
-     //Write your code here.
+        //Write your code here.
+        const user = await Users.findOne({ email });
+        if (user) {
+            res.status(409).json({
+                "status": 'fail',
+                "message": 'User with given Email allready register'
+            });
+        }
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hashedPassword = bcrypt.hashSync(password, salt);
+
+        const newUser = {
+            name,
+            email,
+            role,
+            password: hashedPassword
+        }
+
+        await Users.create(newUser);
+        res.status(200).json({
+            "message": 'User SignedUp successfully',
+            "status": 'success'
+        })
+    } catch (err) {
+        res.status(404).json({
+            "status": 'fail',
+            "message": 'Something went wrong'
+        })
+    }
+
+
 
 }
 
-module.exports = { loginUser , signupUser };
+module.exports = { loginUser, signupUser };
 
